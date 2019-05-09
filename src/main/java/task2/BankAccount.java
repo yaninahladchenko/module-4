8edc5c17020619df9b16e2fe98278b28397e0563
Yanina Hladchenko <yanina.hladchenko@gmail.com>
2019-05-09 18:03:26 +0300

    Task 2 updated

--- src/main/java/task2/BankAccount.java
+++ src/main/java/task2/BankAccount.java
@@ -2,19 +2,11 @@ package task2;
 
 import java.util.Observable;
 
-public class BankAccount extends Observable {
+public abstract class BankAccount extends Observable {
 
     protected double accountBalance;
-    protected double creditFundsFeePercent = 5;
-    protected double personalFundsFeePercent = 1;
     protected double withdrawalLimit;
-    protected boolean isCreditLineAvailable;
-
-    public BankAccount(double accountBalance, double withdrawalLimit, boolean isCreditLineAvailable) {
-        this.accountBalance = accountBalance;
-        this.withdrawalLimit = withdrawalLimit;
-        this.isCreditLineAvailable = isCreditLineAvailable;
-    }
+    protected double feePercent;
 
     public void setAccountBalance(double accountBalance) {
         this.accountBalance = accountBalance;
@@ -23,31 +15,14 @@ public class BankAccount extends Observable {
     }
 
     public void addMoney(double addAmount) {
-        System.out.println("Money added on balance: " + addAmount);
         setAccountBalance(accountBalance + addAmount);
     }
 
-    public double withdrawMoney(double withdrawalAmount) {
-        if (withdrawalAmount + calculateWithdrawalFee(withdrawalAmount) > withdrawalLimit) {
-            System.out.println("It is impossible to get more money than your withdrawal limit: " + withdrawalLimit);
-        } else if (isCreditLineAvailable == false && withdrawalAmount + calculateWithdrawalFee(withdrawalAmount) > accountBalance) {
-            System.out.println("It is impossible to get more money than the account currently has.");
-        } else {
-            System.out.println("Money withdrawn from balance: " + withdrawalAmount);
-            System.out.println("Fee: " + calculateWithdrawalFee(withdrawalAmount));
-            accountBalance = accountBalance - withdrawalAmount - calculateWithdrawalFee(withdrawalAmount);
-            setAccountBalance(accountBalance);
-        }
-        return accountBalance;
-    }
+    public abstract double withdrawMoney(double withdrawalAmount);
 
     public double calculateWithdrawalFee(double withdrawalAmount) {
-        if (isCreditLineAvailable) {
-            return withdrawalAmount * creditFundsFeePercent / 100;
-            //ToDo: fix. In this case it will take 5% fee even if personal funds available.
-        } else {
-            return withdrawalAmount * personalFundsFeePercent / 100;
-        }
+        //ToDo: fix. In this case it will take 5% fee even if personal funds available.
+        return withdrawalAmount * feePercent / 100;
     }
 
 }
--- src/main/java/task2/BankAccountTest.java
+++ src/main/java/task2/BankAccountTest.java
@@ -5,14 +5,14 @@ public class BankAccountTest {
     public static void main(String[] args) {
         Bank bankObserver = new Bank();
 
-        BankAccount debitBankAccountObservable = new BankAccount(0.00, 1000.00, false);
+        BankAccount debitBankAccountObservable = new DebitBankAccount(0.00, 1000.00, 1);
         debitBankAccountObservable.addObserver(bankObserver);
 
         debitBankAccountObservable.addMoney(200);
         debitBankAccountObservable.withdrawMoney(100);
         debitBankAccountObservable.withdrawMoney(99);
 
-        BankAccount creditBankAccountObservable = new BankAccount(0.00, 1000.00, true);
+        BankAccount creditBankAccountObservable = new CreditBankAccount(0.00, 1000.00, 5);
         creditBankAccountObservable.addObserver(bankObserver);
 
         creditBankAccountObservable.withdrawMoney(200);
